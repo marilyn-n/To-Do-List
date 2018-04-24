cb9af16ec1c36c566a3d7b822e4114f05cad2bc4
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-04-23 22:49:31 -0500

    delete button

--- server/routes/new-list.js
+++ server/routes/new-list.js
@@ -1,10 +1,25 @@
 const express = require('express');
 
 const router = express.Router();
+const Item = require('../models/Item');
 
 router.get('/new-list', (req, res) => {
   console.log('logrado');
   res.render('new-list');
 });
+
+router.post('/new-list', (req, res) => {
+  const newItem = new Item({
+    itemName: req.body.itemName,
+  });
+  newItem.save()
+    .then(() => {
+      console.log('usuario guardado!!!');
+      res.redirect('new-list');
+      // res.status(200).json();
+    })
+    .catch(() => console.log('oh oh error'));
+});
+
 
 module.exports = router;
